d5bbf1df4cfd228d4705c4d3b530d1ab1d18b4eb
ThBellucci <40698255+thomas097@users.noreply.github.com>
2022-03-03 14:48:22 +0100

    Delete main.py

--- annotation/main.py
+++
@@ -1,6 +0,0 @@
-from dataloader import DatasetIO
-from interface import Interface
-
-if __name__ == '__main__':
-    dataloader = DatasetIO('datasets/dailydialog.txt')
-    interface = Interface(dataloader)
