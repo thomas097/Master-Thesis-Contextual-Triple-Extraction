f9f49c062a2a118e9d2e3a7872057c5021de4c1f
ThBellucci <40698255+thomas097@users.noreply.github.com>
2022-03-07 13:52:14 +0100

    Delete annotation_tool directory

--- annotation_tool/main.py
+++
@@ -1,6 +0,0 @@
-from dataloader import DataLoader
-from interface import Interface
-
-if __name__ == '__main__':
-    dataloader = DataLoader('datasets/train.json', output_dir='annotations')
-    interface = Interface(dataloader)
