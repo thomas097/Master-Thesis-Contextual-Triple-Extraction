bafe0ab02e0e542a9affbe029b9bdb4c7c2aa0d7
ThBellucci <40698255+thomas097@users.noreply.github.com>
2022-03-12 23:41:35 +0100

    Add files via upload

--- src/model_transformer/run_transformer_pipeline.py
+++ src/model_transformer/run_transformer_pipeline.py
@@ -10,8 +10,8 @@ class AlbertTripleExtractor:
         self._arg_model = ArgumentExtraction(base_model, path=arg_model_path)
         self._scorer_model = TripleScoring(base_model, path=scorer_model_path)
 
-    def extract_triples(self, inputs):
-        y_subjs, y_preds, y_objs, subwords = self._arg_model.predict(input_.split())
+    def extract_triples(self, inputs, confidenec=0.4):
+        y_subjs, y_preds, y_objs, subwords = self._arg_model.predict(inputs.split())
         subjs = bio_tags_to_tokens(subwords, y_subjs.T, one_hot=True)
         preds = bio_tags_to_tokens(subwords, y_preds.T, one_hot=True)
         objs = bio_tags_to_tokens(subwords, y_objs.T, one_hot=True)
@@ -23,12 +23,12 @@ class AlbertTripleExtractor:
         # Score all possible triples
         triple_scores = []
         for subj, pred, obj in product(subjs, preds, objs):
-            entailed, polarity = self._scorer_model.predict(input_.split(), [subj, pred, obj])
+            entailed, polarity = self._scorer_model.predict(inputs.split(), [subj, pred, obj])
             triple_scores.append((entailed[1], (subj, pred, obj, polarity[1])))
 
         # Rank triples from high (entailed) to low (not entailed)
         for entailed, (subj, pred, obj, pol) in sorted(triple_scores, key=lambda x: -x[0]):
-            if entailed:
+            if entailed > confidenec:
                 yield entailed, (subj, pred, obj), pol
 
 
@@ -36,7 +36,8 @@ if __name__ == '__main__':
     model = AlbertTripleExtractor('models/argument_extraction_albert-v2_09_03_2022',
                                   'models/scorer_albert-v2_09_03_2022')
     # Test!
-    example = 'I went to see a movie with Jim <eos> I hate movies. Do you enjoy watching movies? <eos> No, I also do n\'t like animals <eos>'
+    example = 'I do n\'t like animals <eos>'
     print("Input: ", example, '\n')
-    for score, triple, polarity in model.extract_triples(example):
+    for score, triple, polarity_score in model.extract_triples(example):
+        polarity = 'positive' if polarity_score > 0.5 else 'negative'
         print(score, triple, polarity)
