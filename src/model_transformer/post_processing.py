65ca8078525c764235055a50441b15e86d86a1ca
ThBellucci <40698255+thomas097@users.noreply.github.com>
2022-04-20 19:39:06 +0200

    more robust post-processing

--- src/model_transformer/post_processing.py
+++ src/model_transformer/post_processing.py
@@ -1,72 +1,30 @@
-from glob import glob
-import json
 import spacy
 import re
 
 # Rules to map different OIE outputs to the same standard
-MOVE_TO_OBJ = {'VERB PART VERB': [2],
-               'VERB VERB ADP': [1, 2],
-               'VERB VERB PART': [1, 2],
-               'VERB AUX ADP': [1, 2],
-               'VERB PART VERB ADP': [2, 3],
-               'INTJ PART VERB ADP': [2, 3],
-               'INTJ PART VERB': [2],
-               'VERB VERB': [1],
-               'ADP VERB': [1],
-               'VERB to VERB ADP VERB': [2, 3, 4],
-               'VERB to VERB PART VERB': [2, 3, 4],
-               'VERB to VERB VERB': [2, 3],
-               'feel like VERB ADP': [2, 3],
-               'is about VERB': [2],
-               'VERB to VERB PRON ADP': [2, 3, 4],
-               'VERB VERB ADP ADP': [1, 2, 3],
-               'would VERB to VERB': [3],
-               'would VERB VERB': [2],
-               'VERB to VERB ADV ADP': [2, 3, 4],
-               'VERB DET NOUN ADP': [1, 2, 3],
-               'VERB to VERB ADP DET NOUN': [2, 3, 4, 5]}
+PRED_RULES = {'VERB PART VERB': [0, 1],           # "want to go"
+              'VERB VERB ADP': [0],               # "likes walking with"
+              'VERB VERB PART': [0],              # "likes walking to"
+              'VERB AUX ADP': [0],
+              'VERB PART VERB ADP': [0, 1],       # "went to walk with"
+              'VERB VERB': [1],                   # "like walking"
+              'VERB PART VERB PART': [0, 1],      # "like to walk with"
+              'VERB ADP VERB PART': [0, 1],       # "feel like going to"
+              'VERB PART VERB VERB': [0, 1],
+              'VERB ADP VERB VERB': [0, 1],
+              'AUX ADP': [0, 1],                  # "If is about [a movie]"
+              'AUX VERB VERB': [0, 1],            # "Must be [having]"
+              'VERB ADP VERB ADP': [0, 1],
+              'VERB PART VERB PRON ADP': [0, 1]}
 
-# Simplification rules which strip off auxiliaries from predicates
-REMOVE_AUXILIARIES = {"am VERB": [0],
-                      "'m VERB": [0],
-                      "' m VERB": [0, 1],
-                      "'m ADJ ADP": [0],
-                      "able to VERB ADP": [0, 1],
-                      "AUX ADJ ADP": [0],
-                      "are VERB": [0],
-                      "is VERB": [0],
-                      "do VERB": [0],
-                      "'ll VERB": [0],
-                      "is VERB ADP": [0],
-                      "will VERB ADP": [0],
-                      "will VERB PRON": [0],
-                      "been VERB ADP": [0]}
+
+# Simplification rules which strip off auxiliaries from predicates (that signal certainty!)
+AUXILIARIES = ["am", "'m", "' m", "are", "is", "do", "'ll", "will", "be", "will", "been"]
 
 
 class PostProcessor:
     def __init__(self):
         self._nlp = spacy.load('en_core_web_sm')
-
-    def _apply_rule(self, tokens, tags, rules):
-        """ Identifies rule to transform token sequence.
-        """
-        for rule in rules.keys():
-            # Unequal length is never a match
-            rule_items = rule.split()
-            if len(rule_items) != len(tokens):
-                continue
-
-            # Check match with rule
-            match = True
-            for i, item in enumerate(rule_items):
-                if item != tokens[i] and item != tags[i]:
-                    match = False
-                    break
-
-            # Return if match
-            if match:
-                return rules[rule]
-        return []
 
     @staticmethod
     def _decontract(phrase, is_predicate=False):
@@ -88,31 +46,45 @@ class PostProcessor:
         # Remove double spaces if any
         return re.sub(' +', ' ', phrase.strip())
 
+    def _pos_sequence(self, predicate):
+        """ Returns the POS sequence of a given predicate string. We add
+            a token "I" to ensure correct pos sequence.
+        """
+        return ' '.join([t.pos_ for t in self._nlp("I " + predicate)][1:])
+
     def format(self, triple):
         # Fix contractions
         subj = self._decontract(triple[0])
-        rel_ = self._decontract(triple[1], is_predicate=True)
-        obj_ = self._decontract(triple[2])
+        pred = self._decontract(triple[1], is_predicate=True)
+        obj = self._decontract(triple[2])
 
         # Get token sequence of arguments
-        rel_tags = [t.pos_ for t in self._nlp(rel_)]
-        subj = [t.lower_ for t in self._nlp(subj)]
-        rel_ = [t.lower_ for t in self._nlp(rel_)]
-        obj_ = [t.lower_ for t in self._nlp(obj_)]
+        pred_tags = self._pos_sequence(pred)
+        obj_tags = self._pos_sequence(obj)
+        pred = [t.lower_ for t in self._nlp(pred)]
+        obj = [t.lower_ for t in self._nlp(obj)]
 
-        # Are there any auxiliaries that need to go?
-        remove_idx = self._apply_rule(rel_, rel_tags, REMOVE_AUXILIARIES)
+        # Remove auxiliaries if there is an alternative verb or auxiliary
+        if len(pred) > 1:
+            for aux in AUXILIARIES:
+                if pred[0] == aux and pred_tags.split(' ')[1] in ['AUX', 'VERB']:
+                    pred = pred[1:]
+                    pred_tags = self._pos_sequence(' '.join(pred))
 
-        # What should be moved to the object
-        move_idx = self._apply_rule(rel_, rel_tags, MOVE_TO_OBJ)
+        # If object starts with PART or ADP; move token to predicate
+        if obj_tags.split(' ')[0] in ['PART', 'ADP']:
+            pred = pred + [obj.pop(0)]
+            pred_tags = self._pos_sequence(' '.join(pred))
 
-        # Apply remove and move rules
-        subj2 = ' '.join(subj)
-        pred2 = ' '.join([t for i, t in enumerate(rel_) if i not in move_idx + remove_idx])
-        obj2_ = ' '.join([t for i, t in enumerate(rel_) if i in move_idx] + obj_)
-        return subj2, pred2, obj2_
+        # Simplify predicate by moving parts to object
+        for rule, pred_idx in PRED_RULES.items():
+            if pred_tags.startswith(rule):
+                obj = [t for i, t in enumerate(pred) if i not in pred_idx] + obj
+                pred = [t for i, t in enumerate(pred) if i in pred_idx]
+
+        return subj, ' '.join(pred), ' '.join(obj)
 
 
 if __name__ == '__main__':
     pp = PostProcessor()
-    print(pp.format(('SPEAKER1 \'s dog', 'likes to \'ve', 'speaker2 \'s bone')))
+    print(pp.format(('she', 'must be have a tremendously', 'progressive evening')))
