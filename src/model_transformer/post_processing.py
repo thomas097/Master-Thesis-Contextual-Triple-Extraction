979866e815924daabb366dc114cb9db2681a6a47
ThBellucci <40698255+thomas097@users.noreply.github.com>
2022-04-20 13:36:32 +0200

    Cleaned up post-processor for baselines

--- src/model_transformer/post_processing.py
+++ src/model_transformer/post_processing.py
@@ -3,32 +3,14 @@ import json
 import spacy
 import re
 
-
-def load_annotations(path):
-    for fname in glob(path + '/*.json'):
-        with open(fname, 'r', encoding='utf-8') as file:
-            yield json.load(file)
-
-
-def get_triples(annotation):
-    tokens = annotation['tokens']
-    for i, (subj, pred, obj, _, _) in enumerate(annotation['annotations']):
-        if subj or pred or obj:
-            yield i, format_triple(subj, pred, obj, tokens)
-
-
-def format_triple(subj, pred, obj, tokens):
-    subj = ' '.join([tokens[i][j] for i, j in subj])
-    pred = ' '.join([tokens[i][j] for i, j in pred])
-    obj = ' '.join([tokens[i][j] for i, j in obj])
-    return subj, pred, obj
-
-
-MOVE_TO_OBJ = {'VERB to VERB': [2],
+# Rules to map different OIE outputs to the same standard
+MOVE_TO_OBJ = {'VERB PART VERB': [2],
                'VERB VERB ADP': [1, 2],
                'VERB VERB PART': [1, 2],
                'VERB AUX ADP': [1, 2],
-               'VERB to VERB ADP': [2, 3],
+               'VERB PART VERB ADP': [2, 3],
+               'INTJ PART VERB ADP': [2, 3],
+               'INTJ PART VERB': [2],
                'VERB VERB': [1],
                'ADP VERB': [1],
                'VERB to VERB ADP VERB': [2, 3, 4],
@@ -36,7 +18,6 @@ MOVE_TO_OBJ = {'VERB to VERB': [2],
                'VERB to VERB VERB': [2, 3],
                'feel like VERB ADP': [2, 3],
                'is about VERB': [2],
-               'went out': [1],
                'VERB to VERB PRON ADP': [2, 3, 4],
                'VERB VERB ADP ADP': [1, 2, 3],
                'would VERB to VERB': [3],
@@ -45,6 +26,7 @@ MOVE_TO_OBJ = {'VERB to VERB': [2],
                'VERB DET NOUN ADP': [1, 2, 3],
                'VERB to VERB ADP DET NOUN': [2, 3, 4, 5]}
 
+# Simplification rules which strip off auxiliaries from predicates
 REMOVE_AUXILIARIES = {"am VERB": [0],
                       "'m VERB": [0],
                       "' m VERB": [0, 1],
@@ -53,11 +35,13 @@ REMOVE_AUXILIARIES = {"am VERB": [0],
                       "AUX ADJ ADP": [0],
                       "are VERB": [0],
                       "is VERB": [0],
+                      "do VERB": [0],
                       "'ll VERB": [0],
                       "is VERB ADP": [0],
                       "will VERB ADP": [0],
                       "will VERB PRON": [0],
                       "been VERB ADP": [0]}
+
 
 class PostProcessor:
     def __init__(self):
@@ -85,7 +69,8 @@ class PostProcessor:
         return []
 
     @staticmethod
-    def _decontract(phrase):
+    def _decontract(phrase, is_predicate=False):
+        # Decontract of common contractions and fragments
         phrase = re.sub(r"n\'t", " not", phrase)
         phrase = re.sub(r"\'re", " are", phrase)
         phrase = re.sub(r"\'d", " would", phrase)
@@ -95,28 +80,39 @@ class PostProcessor:
         phrase = re.sub(r"\'m", " am", phrase)
         phrase = re.sub(r" wo ", "will ", ' ' + phrase + ' ')
         phrase = re.sub(r" ca ", "can ", ' ' + phrase + ' ')
-        return phrase.strip()
+
+        # 's in predicate is always 'to be'
+        if is_predicate:
+            phrase = re.sub(r"\'s", " is", phrase)
+
+        # Remove double spaces if any
+        return re.sub(' +', ' ', phrase.strip())
 
     def format(self, triple):
         # Fix contractions
         subj = self._decontract(triple[0])
-        pred = self._decontract(triple[1])
+        rel_ = self._decontract(triple[1], is_predicate=True)
         obj_ = self._decontract(triple[2])
 
         # Get token sequence of arguments
-        pred_tags = [t.pos_ for t in self._nlp(pred)]
+        rel_tags = [t.pos_ for t in self._nlp(rel_)]
         subj = [t.lower_ for t in self._nlp(subj)]
-        pred = [t.lower_ for t in self._nlp(pred)]
+        rel_ = [t.lower_ for t in self._nlp(rel_)]
         obj_ = [t.lower_ for t in self._nlp(obj_)]
 
         # Are there any auxiliaries that need to go?
-        remove_idx = self._apply_rule(pred, pred_tags, REMOVE_AUXILIARIES)
+        remove_idx = self._apply_rule(rel_, rel_tags, REMOVE_AUXILIARIES)
 
         # What should be moved to the object
-        move_idx = self._apply_rule(pred, pred_tags, MOVE_TO_OBJ)
+        move_idx = self._apply_rule(rel_, rel_tags, MOVE_TO_OBJ)
 
         # Apply remove and move rules
         subj2 = ' '.join(subj)
-        pred2 = ' '.join([t for i, t in enumerate(pred) if i not in move_idx + remove_idx])
-        obj2_ = ' '.join([t for i, t in enumerate(pred) if i in move_idx] + obj_)
+        pred2 = ' '.join([t for i, t in enumerate(rel_) if i not in move_idx + remove_idx])
+        obj2_ = ' '.join([t for i, t in enumerate(rel_) if i in move_idx] + obj_)
         return subj2, pred2, obj2_
+
+
+if __name__ == '__main__':
+    pp = PostProcessor()
+    print(pp.format(('SPEAKER1 \'s dog', 'likes to \'ve', 'speaker2 \'s bone')))
