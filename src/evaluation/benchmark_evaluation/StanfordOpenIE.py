d2df7c88945188b3fc199e523b52e1c445a87e6e
ThBellucci <40698255+thomas097@users.noreply.github.com>
2022-04-26 20:56:27 +0200

    Cleanup of Stanford OpenIE

--- src/evaluation/benchmark_evaluation/StanfordOpenIE.py
+++ src/evaluation/benchmark_evaluation/StanfordOpenIE.py
@@ -1,42 +1,64 @@
-import sys
-sys.path.append('../../model_transformer')
-from post_processing import PostProcessor
-
 import os
 import re
 import subprocess
+import spacy
+
+from post_processing import PostProcessor
 
 
-PRONOUNS = [(('i', 'me', 'myself', 'we', 'us', 'ourselves', 'my'), 'speaker1'),
-            (("my", 'mine', 'our'), "speaker1 's"),
-            (('you', 'yourself', 'yourselves'), 'speaker2'),
-            (('your', 'yours'), "speaker2 's")]
-
-NEG_TOKENS = ['not', "n't", 'never', "'t", 'neither']
+PRONOUNS = {"speaker1": ['i', 'me', 'myself', 'we', 'us', 'ourselves'],
+            "speaker2": ['you', 'yourself', 'yourselves'],
+            "speaker1 's": ["my", 'mine', 'our'],
+            "speaker2 's": ['your', 'yours']}
 
 
 class StanfordOpenIEBaseline:
-    def __init__(self, path="stanford-corenlp-latest/stanford-corenlp-4.4.0", speaker1='speaker1', speaker2='speaker2', sep='<eos>'):
-        self._path = path
+    def __init__(self, path="stanford-corenlp-latest/stanford-corenlp-4.4.0", spacy_model='en_core_web_sm', speaker1='speaker1', speaker2='speaker2', sep='<eos>'):
+        """ Constructor of the Stanford OpenIE baseline.
+
+        :param path:         Path to the 'stanford-corenlp-<VERSION>' directory
+        :param spacy_model:  SpaCy model (default: en_core_web_sm)
+        :param speaker1:     Name of the user (default: speaker1)
+        :param speaker2:     Name of the system (default: speaker2)
+        :param sep:          Separator used to delimit dialogue turns (default: <eos>)
+        """
         self._post_processor = PostProcessor()
+        self._nlp = spacy.load(spacy_model)
         self._speaker1 = speaker1
         self._speaker2 = speaker2
+        self._path = path
         self._sep = sep
 
-    @staticmethod
-    def _negation_in_predicate(pred):
-        for token in NEG_TOKENS:
-            if token in pred:
-                pred = pred.replace(token + ' ', '')
-                return 'negative', pred
-        return 'positive', pred
+    def _extract_perspective(self, subj, pred, obj):
+        """ Identifies negation of triple using SpaCy (not supported by Stanford OpenIE).
 
-    @staticmethod
-    def _disambuate_pronouns(turn, turn_id):
+        :param subj: subject of the triple
+        :param pred: predicate of the triple, containing potential negation
+        :param obj:  object of triple
+        :return:
+        """
+        # Identify tokens marking negation
+        doc = self._nlp('{} {} {}'.format(subj, pred, obj))
+        negations = [t.lower_ for t in doc if t.dep_ == 'neg']
+
+        if not negations:
+            return subj, pred, obj, 'positive'
+
+        # If negated, place negation in perspective
+        pred = ' '.join([t.lower_ for t in doc if t.dep_ != 'neg'])
+        return subj, pred, obj, 'negative'
+
+    def _disambiguate_pronouns(self, arg, turn_id):
+        """ Replaces 'you' and 'I' by the corresponding referent (speaker1 or speaker2)
+
+        :param arg:     String representing an argument of a triple
+        :param turn_id: Index of the turn in the dialogue (0=speaker1, 1=speaker2, 2=speaker1, etc.)
+        :return:        Argument with personal and possessive pronouns replaced
+        """
         # Split contractions and punctuation from tokens
-        turn = ' %s ' % ' '.join(re.findall("[\w\d-]+|'\w|[.,!?]", turn.lower()))
+        arg = re.findall("[\w\d-]+|'\w|[.,!?]", arg.lower())
 
-        for pronouns, speaker_id in PRONOUNS:
+        for speaker_id, pronouns in PRONOUNS.items():
             # Swap speakers for uneven turns
             if turn_id % 2 == 1:
                 if 'speaker1' in speaker_id:
@@ -44,45 +66,65 @@ class StanfordOpenIEBaseline:
                 else:
                     speaker_id = speaker_id.replace('speaker2', 'speaker1')
 
+            # Replace by referent name
+            speaker_id = speaker_id.replace('speaker1', self._speaker1).replace('speaker2', self._speaker2)
+
             # Replace pronoun occurrences with speaker_ids
-            for pron in pronouns:
-                if ' %s ' % pron in turn:
-                    turn = turn.replace(' %s ' % pron, ' ' + speaker_id + ' ')
-        return turn
+            arg = [t if t not in pronouns else speaker_id for t in arg]
+
+        return ' '.join(arg)
 
     def extract_triples(self, dialogue, verbose=False):
-        # Write dialogue out to file with disambiguated speakers
-        lines = []
-        for turn_id, turn in enumerate(dialogue.split(self._sep)):
-            line = self._disambuate_pronouns(turn.strip(), turn_id).strip()
-            lines.append(line)
+        """ Extracts a set of triples from an <eos>-delimited dialogue
 
-        with open('tmp.txt', 'w', encoding='utf-8') as file:
-            file.write('\n'.join(lines))
-
-        # Capture OpenIE output from stdout
-        wd = os.getcwd()
-        os.chdir(self._path)
-
-        if verbose:
-            out = subprocess.check_output('java -mx1g -cp "*" edu.stanford.nlp.naturalli.OpenIE ../../tmp.txt')
-        else:
-            out = subprocess.check_output('java -mx1g -cp "*" edu.stanford.nlp.naturalli.OpenIE ../../tmp.txt',
-                                          stderr=subprocess.PIPE)
-        os.chdir(wd)
-        lines = out.decode('UTF-8').strip().replace('\r', '').split('\n')
-        lines = [line for line in lines if line.strip() != '']
-
-        # Extract triple and confidence
+        :param dialogue: <eos>-delimited dialogue
+        :param verbose:  Whether to print the messages of the system (default: False)
+        :return:         A set of tuples in the form of (confidence, (subj, pred, obj, polarity))
+        """
+        # Where to output warnings, messages, etc.
+        stderr = subprocess.PIPE if not verbose else None
         triples = []
-        for line in lines:
-            conf, subj, pred, obj = line.split('\t')
-            polar, rel = self._negation_in_predicate(pred)
-            conf = float(conf)
-            triples.append((conf, self._post_processor.format((subj, rel, obj)) + (polar,)))
+
+        # Analyze turns separately
+        for turn_id, turn in enumerate(dialogue.split(self._sep)):
+
+            # Change working directory to run java from root
+            wd = os.getcwd()
+            os.chdir(self._path)
+
+            # Write turn out to file
+            with open('tmp.txt', 'w', encoding='utf-8') as file:
+                file.write(turn.strip())
+
+            out = subprocess.check_output('java -mx1g -cp "*" edu.stanford.nlp.naturalli.OpenIE tmp.txt -triple.strict false', stderr=stderr)
+            if verbose:
+                print('output:', out)
+            os.chdir(wd)
+
+            # Convert b' string to plain text
+            lines = out.decode('UTF-8').strip().replace('\r', '').split('\n')
+
+            # Extract triples from output lines
+            for line in lines:
+                if line.strip():
+                    conf, subj, pred, obj = line.split('\t')
+                    conf = float(conf)
+
+                    # Determine polarity using SpaCy
+                    subj, pred, obj, polar = self._extract_perspective(subj, pred, obj)
+
+                    # Disambiguate You/I
+                    subj = self._disambiguate_pronouns(subj, turn_id)
+                    pred = self._disambiguate_pronouns(pred, turn_id)
+                    obj = self._disambiguate_pronouns(obj, turn_id)
+
+                    # Make sure the output conforms to standard
+                    subj, pred, obj = self._post_processor.format((subj, pred, obj))
+
+                    triples.append((conf, (subj, pred, obj, polar)))
         return triples
 
 
 if __name__ == '__main__':
-    openie = StanfordOpenIEBaseline()
-    print(openie.extract_triples('The beer is cold', verbose=True))
+    baseline = StanfordOpenIEBaseline(speaker1='alice', speaker2='bob')
+    print(baseline.extract_triples('My beer is cold <eos> I am very tired', verbose=True))
