25a0fef2038772d5519b365ae3ffb09bdd340b20
ThBellucci <40698255+thomas097@users.noreply.github.com>
2022-04-26 15:52:40 +0200

    Increased test set size and linked rels to WordNet

--- src/model_transformer/predicate_normalization/evaluate_predicate_normalization.py
+++ src/model_transformer/predicate_normalization/evaluate_predicate_normalization.py
@@ -3,13 +3,15 @@ from sklearn.metrics import classification_report
 
 
 def speaker_to_pronoun(arg, slot='subj'):
-    """ Maps back from speaker ids to pronouns to help BERT with creating good
-        embeddings (speaker* is not in the vocabulary).
+    """ Maps back from speaker ids to pronouns. Helps BERT with creating
+        better embeddings as speaker* is not in the vocabulary.
     """
-    if slot == 'subj':
-        arg = arg.replace('speaker1', 'I').replace('speaker2', 'I')
-    else:
-        arg = arg.replace('speaker1', 'you').replace('speaker2', 'you')
+    sing_pron = 'I' if slot == 'subj' else 'you'
+    poss_pron = 'my' if slot == 'subj' else 'your'
+    arg = arg.replace("speaker1 's", poss_pron)
+    arg = arg.replace("speaker2 's", poss_pron)
+    arg = arg.replace("speaker1", sing_pron)
+    arg = arg.replace("speaker2", sing_pron)
     return arg.strip()
 
 
@@ -20,13 +22,18 @@ def load_test_data(fname):
     triples = []
     labels = []
     with open(fname, 'r', encoding='utf-8') as file:
-        for line in file:
+        for i, line in enumerate(file):
             if line.strip() and not line.startswith('#'):
-                subj, pred, obj, _, norm_pred = line.strip().split(',')
-                triples.append((speaker_to_pronoun(subj, slot='subj'),
-                                speaker_to_pronoun(pred, slot='pred'),
-                                speaker_to_pronoun(obj, slot='obj')))
-                labels.append(norm_pred)
+                try:
+                    subj, pred, obj, _, norm_pred = line.strip().split(',')
+                    triples.append((speaker_to_pronoun(subj, slot='subj'),
+                                    speaker_to_pronoun(pred, slot='pred'),
+                                    speaker_to_pronoun(obj, slot='obj')))
+                    labels.append(norm_pred)
+                except:
+                    print('Error at %s: %s' % (i, line))
+
+    print("Found %s test samples" % len(triples))
     return triples, labels
 
 
@@ -38,8 +45,8 @@ def evaluate(normalizer, test_triples, test_labels):
         norm_pred, conf = normalizer.normalize(*triple)
         pred_labels.append(norm_pred)
         print('%s (%s)' % (triple, conf))
-        print('True:', norm_pred)
-        print('Pred:', test_labels[i])
+        print('True:', test_labels[i])
+        print('Pred:', norm_pred)
         print()
 
     print(classification_report(test_labels, pred_labels, zero_division=0))
@@ -47,5 +54,5 @@ def evaluate(normalizer, test_triples, test_labels):
 
 if __name__ == '__main__':
     test_triples, test_labels = load_test_data('test_exemplars.txt')
-    normalizer = PredicateNormalizer('canonical_exemplars.txt')
+    normalizer = PredicateNormalizer('training_exemplars.txt')
     evaluate(normalizer, test_triples, test_labels)
--- src/model_transformer/predicate_normalization/predicate_normalization.py
+++ src/model_transformer/predicate_normalization/predicate_normalization.py
@@ -1,8 +1,11 @@
-from transformers import AutoTokenizer, AutoModel
 import torch
 import numpy as np
 from tqdm import tqdm
+from transformers import AutoTokenizer, AutoModel
 from sklearn.neighbors import KNeighborsClassifier
+
+import spacy
+from lemminflect import getInflection
 
 
 class BERT:
@@ -38,8 +41,9 @@ class BERT:
 
 
 class PredicateNormalizer:
-    def __init__(self, exemplar_file, base_model='bert-base-uncased', k=3, min_conf=0.6):
+    def __init__(self, exemplar_file, base_model='bert-base-uncased', k=3, min_conf=0.3):
         self._model = BERT(base_model)
+        self._nlp = spacy.load('en_core_web_sm')
         self._knn = KNeighborsClassifier(n_neighbors=k, metric=self._cosine_dist, weights='distance', algorithm='brute')
         self._fit_knn(exemplar_file)
         self._min_conf = min_conf
@@ -48,19 +52,37 @@ class PredicateNormalizer:
         print('\t- Fitting kNN')
         X, y = [], []
         with open(path, 'r', encoding='utf-8') as file:
-            for line in tqdm(file):
+            for i, line in tqdm(enumerate(file)):
                 line = line.strip()
                 if line and not line.startswith('#'):
-                    norm_pred, subj, pred, obj = [t.strip() for t in line.strip().split(',')]
-                    # Precompute embeddings
-                    X.append(self._model.get_embedding(subj, pred, obj))
-                    y.append(norm_pred)
+                    try:
+                        norm_pred, subj, pred, obj = [t.strip() for t in line.strip().split(',')]
+                        # Precompute embeddings
+                        X.append(self._model.get_embedding(subj, pred, obj))
+                        y.append(norm_pred)
+                    except:
+                        print('Error in line %s: %s' % (i, line))
         self._knn.fit(X, y)
 
     @staticmethod
     def _cosine_dist(x, y):
         sim = x.dot(y) / (np.linalg.norm(x) * np.linalg.norm(y))
         return 1 - sim
+
+    def _normalize_surface_form(self, pred):
+        pred_tokens = []
+        for token in list(self._nlp('I ' + pred))[1:]:
+            # Map verbs to third-person singular
+            if token.pos_ in ['VERB', 'INTJ', 'AUX']:
+                inflections = getInflection(token.lemma_, tag='VBZ')  # may yield multiple inflections still
+                token_string = token.lower_ if not inflections else inflections[0]
+            else:
+                token_string = token.lower_
+
+            pred_tokens.append(token_string)
+
+        # Use '_' as delimiter
+        return '_'.join(pred_tokens)
 
     def normalize(self, subj, pred, obj):
         # Get contextual embedding of predicate
@@ -76,13 +98,13 @@ class PredicateNormalizer:
         if probs[i] > self._min_conf:
             return self._knn.classes_[i], probs[i]
 
-        # Replace spaces with underscores
-        pred = pred.replace(' ', '_')
+        # If no match is found, map predicate to 3rd-person singular
+        pred = self._normalize_surface_form(pred)
         return pred, probs[i]
 
 
 if __name__ == '__main__':
-    pred_norm = PredicateNormalizer('canonical_exemplars.txt')
+    pred_norm = PredicateNormalizer('training_exemplars.txt')
     while True:
         subj = input('subj: ')
         pred = input('pred: ')
