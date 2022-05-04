213fa92a03697aec9cbbe82145f8db144960cd47
ThBellucci <40698255+thomas097@users.noreply.github.com>
2022-05-04 10:19:14 +0200

    Add files via upload

---
+++ src/dataset/data_statistics.py
@@ -0,0 +1,79 @@
+import glob
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def load_annotations(path):
+    annotations = {}
+    for fname in glob.glob(path + '/*.json'):
+        with open(fname, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+            if not data['skipped']:
+                data['tokens'] = [[t for t in turn if t != '[unk]'] for turn in data['tokens']]
+                annotations[fname] = data
+    return annotations
+
+
+def piechart(sizes, labels, explode_size=0.02):
+    explode = np.zeros(len(sizes))
+    explode[1] = explode_size
+
+    fig1, ax1 = plt.subplots()
+    ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+            shadow=False, startangle=90)
+    ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+    plt.show()
+
+
+def show_stats(path):
+    directories = glob.glob(path + '/*')
+    print('Number of annotators:', len(directories))
+
+    print('\nAnnotator contributions:')
+    all_annotations = {}
+    for dir_ in directories:
+        annotations = load_annotations(dir_)
+        all_annotations.update(annotations)
+        print(dir_, ' N =', len(annotations))
+
+    print('\nDataset contributions:')
+    dataset_count = {'circa': 0, 'daily_dialogs': 0, 'personachat': 0}
+    for fname in list(all_annotations.keys()):
+        for dataset_name in dataset_count.keys():
+            if dataset_name in fname:
+                dataset_count[dataset_name] += 1
+    for dataset_name, count in dataset_count.items():
+        print(dataset_name, count)
+
+    piechart((dataset_count['personachat'], dataset_count['daily_dialogs'], dataset_count['circa']),
+             ('PersonaChat', 'DailyDialogs', 'Circa'))
+
+    print('\nAggregate statistics:')
+    dialogue_stats = {'tokens in turn': [], 'tokens in dialogue': [], 'triples in dialogue': []}
+
+    for sample in all_annotations.values():
+        # Count length of individual turns (in tokens)
+        for turn in sample['tokens']:
+            dialogue_stats['tokens in turn'].append(len(turn))
+
+        # Count length of dialogue (in tokens)
+        dialogue_stats['tokens in dialogue'].append(sum([len(t) for t in sample['tokens']]))
+
+        # Count number of triples for dialogue
+        num_triples = sum([any(triple) for triple in sample['annotations']])
+        dialogue_stats['triples in dialogue'].append(num_triples)
+
+    print('#Dialogs:', len(all_annotations))
+    print('#Turns per dialog:', 3)
+    print('#Turns:', len(dialogue_stats['tokens in turn']))
+    print('#Tokens:', sum(dialogue_stats['tokens in turn']))
+    print('#Triples:', sum(dialogue_stats['triples in dialogue']))
+    print('#Triples per dialogue:', np.mean(dialogue_stats['triples in dialogue']))
+    print()
+    print('Avg. tokens per turn:', np.mean(dialogue_stats['tokens in turn']))
+    print('Avg. tokens in dialogue:', np.mean(dialogue_stats['tokens in dialogue']))
+
+
+if __name__ == '__main__':
+    show_stats('annotations')
---
+++ src/dataset/sample_annotation_batches.py
@@ -0,0 +1,94 @@
+import os
+import json
+import random
+
+random.seed(2)
+
+
+if __name__ == '__main__':
+    SAMPLES_PER_BATCH = 200
+    NUM_SHARED = 4
+    START_SHARING = 40
+    NUM_SHARED_SAMPLES = 30
+    PILOT_BATCH = 20
+    NUM_BATCHES = 20
+    DATASET = 'merged_trainval_unannotated.json'
+
+    # Create directory to store batches into
+    if not os.path.exists('batches'):
+        os.mkdir('batches')
+
+    # Load full dataset
+    with open(DATASET, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+
+    # Remove duplicates (just in case)
+    used_ids = set()
+    new_data = []
+    for sample in data:
+        if sample['id'] not in used_ids:
+            new_data.append(sample)
+        used_ids.add(sample['id'])
+    data = new_data
+
+    # Divide subset of N * K samples into K batches
+    batches = []
+    samples = random.sample(data, SAMPLES_PER_BATCH * NUM_BATCHES)
+    for i in range(NUM_BATCHES):
+        j = i * SAMPLES_PER_BATCH
+        batch = samples[j:j + SAMPLES_PER_BATCH]
+        batches.append(batch)
+
+    # Save dialogs (with each 4 batches containing some overlap)
+    used_ids = set()
+    for i in range(0, NUM_BATCHES, NUM_SHARED):
+        group_of_batches = batches[i:i + NUM_SHARED] # 4 batches
+
+        # Determine samples that should be shared
+        shared_batch = group_of_batches[0][START_SHARING:START_SHARING + NUM_SHARED_SAMPLES]  # share middle 30 of first batch
+
+        for j, batch in enumerate(group_of_batches):
+            # Add shared batch to current batch
+            batch[START_SHARING:START_SHARING + NUM_SHARED_SAMPLES] = shared_batch
+
+            # Register samples
+            for sample in batch:
+                used_ids.add(sample['id'])
+
+            # Save to file
+            with open('batches/batch_%s.json' % str(i + j), 'w') as file1:
+                json.dump(batch, file1, indent=4)
+
+    print("{} samples divided over {} batches".format(len(used_ids), NUM_BATCHES))
+
+
+    # Create batch will all remaining samples
+    remaining_batch = [sample for sample in data if sample['id'] not in used_ids]
+    random.shuffle(remaining_batch)
+
+    pilot_batch = []
+    if len(remaining_batch) > PILOT_BATCH:
+        pilot_batch = remaining_batch[:PILOT_BATCH]
+        remaining_batch = remaining_batch[PILOT_BATCH:]
+    print("{} in pilot batch".format(len(pilot_batch)))
+    print("{} remaining".format(len(remaining_batch)))
+
+    with open('batches/pilot_batch.json', 'w') as file:
+        json.dump(pilot_batch, file, indent=4)
+
+    with open('batches/remaining_batch.json', 'w') as file:
+        json.dump(remaining_batch, file, indent=4)
+
+    # Some dataset statistics
+    print('\n### Sampling statistics ###')
+    dataset_balance = {'Circa':0, 'PersonaChat':0, 'DailyDialogs':0}
+    for sample in data:
+        if 'circa' in sample['id']:
+            dataset_balance['Circa'] += 1
+        if 'personachat' in sample['id']:
+            dataset_balance['PersonaChat'] += 1
+        if 'daily_dialogs' in sample['id']:
+            dataset_balance['DailyDialogs'] += 1
+    print(dataset_balance)
+
+
